5eb6ba5a2781a9e40c3a66378ac20d9bd7bef2c5
BryanSummers14 <briansummers14@gmail.com>
2018-02-21 15:26:42 -0700

    Forgot semi colon

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 //clicks button to continue session if session timeout window pops up
-var continueButton
+var continueButton;
 if (continueButton = document.getElementById('continueButton')) {
     continueButton.click();
 }
